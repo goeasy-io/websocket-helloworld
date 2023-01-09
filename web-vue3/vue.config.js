eb2e27ae71267d86aba3fa3f7e4a66c3469846b3
BettyWbb <betty@goeasy.io>
2023-01-09 16:45:00 +0800

    port

--- web-vue3/vue.config.js
+++ web-vue3/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/',
+  //只有Https才能使用录音
+  devServer : {
+    https : true,
+    port : 9008
+  }
 })
