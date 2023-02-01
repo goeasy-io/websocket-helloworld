61d70bfc3b28f9ee57a8838e6bbf2c0e23db399b
19302819013 <support@goeasy.io>
2023-02-01 17:40:34 +0800

    [CI-build.sh] 将版本号升级为：v1.0.4-2，为下个版本做准备

---
+++ ftp-upload.js
@@ -0,0 +1,30 @@
+const FtpDeploy = require("ftp-deploy");
+const packageJson = require('./package.json');
+const ftpDeploy = new FtpDeploy();
+
+let argvs = process.argv.slice(2);
+let host = argvs[0];
+let username = argvs[1];
+let password = argvs[2];
+
+let config = {
+    user: username,
+    password: password,
+    host: host,
+    port: 21,
+    localRoot: "./dist",
+    remoteRoot: "/htdocs/docs",
+    include: ["*", "**/*"],
+    deleteRemote: false,
+    forcePasv: true,
+    sftp: false
+};
+console.log("config:", config);
+ftpDeploy
+    .deploy(config)
+    .then(res => console.log("finished:", res))
+    .catch(err => console.log(err));
+
+ftpDeploy.on("uploading", function(data) {
+    console.log('uploading - ', data.filename);
+});
