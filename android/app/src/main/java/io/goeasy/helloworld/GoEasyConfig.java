6c2291002bd9e51d4d084cd2141f59c11f800d50
jiacai <jiacai@goeasy.io>
2023-05-23 16:14:27 +0800

    1.新增android hello world

---
+++ android/app/src/main/java/io/goeasy/helloworld/GoEasyConfig.java
@@ -0,0 +1,6 @@
+package io.goeasy.helloworld;
+
+public class GoEasyConfig {
+    public static String host = "hangzhou.goeasy.io"; //若是新加坡区域：singapore.goeasy.io
+    public static String appkey = "BC-xxx"; //您的common key
+}
