999d17b9e93d4bc3cc64dd030f4a76b0cecdeb58
ZhangHui <jiacai@goeasy.io>
2023-05-23 17:07:36 +0800

    删除不需要的代码

--- android/app/src/main/java/io/goeasy/helloworld/GoEasyConfig.java
+++ android/app/src/main/java/io/goeasy/helloworld/GoEasyConfig.java
@@ -2,5 +2,5 @@ package io.goeasy.helloworld;
 
 public class GoEasyConfig {
     public static String host = "hangzhou.goeasy.io"; //若是新加坡区域：singapore.goeasy.io
-    public static String appkey = "BC-2f6f21e68fa64ce5a499b01782238fc5"; //您的common key
+    public static String appkey = "BC-xxxx"; //您的common key
 }
